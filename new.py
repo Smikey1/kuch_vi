feaeb1f163b5fa1b11a36ce6a2f80ae8fe0a3458
Smikey <smikeywork@gmail.com>
2022-08-11 11:49:08 +0545

    updated more about string

--- new.py
+++ new.py
@@ -4,4 +4,9 @@
 It represent a multi-line comment
 We can write any thing here
 '''
- 
+
+a = 5   #here, a is a variable which stores/holds integer type value i.e 5
+naam = "Kasmi"   #here, naam is a variable which stores/holds string type value i.e "Kasmi"
+
+# And in python string are always inside single or double quotes: 
+# For example: "kasmi"   OR   'kasmi'
