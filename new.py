88354a721452990edb6bc67a302462307fac78dd
Smikey <smikeywork@gmail.com>
2022-08-16 12:00:50 +0545

    :pencil2: More about function

--- new.py
+++ new.py
@@ -10,3 +10,33 @@ naam = "Kasmi"   #here, naam is a variable which stores/holds string type value 
 
 # And in python string are always inside single or double quotes: 
 # For example: "kasmi"   OR   'kasmi'
+
+# creation of function and calling of function
+
+'''
+syntax:
+
+#creating function 
+def function_name():
+    # you can do any thing here
+    return ...... something
+
+#calling function
+function_name()
+
+'''
+
+# example:
+
+#creating function
+def duita_num_joda():
+    sum = 3+5
+    print(sum)
+    return sum
+
+#calling function with their name
+duita_num_joda()
+
+# taking input from user:
+my_name=input("Please enter your name-->: ")
+print("Ma sanga stored bhako value yei ho-->",my_name)
