9b3fcd2da17513b460fe6fa6ecc4437ce87b4597
Smikey <smikeywork@gmail.com>
2022-08-21 12:13:06 +0545

    Atm machine

--- new.py
+++ new.py
@@ -38,5 +38,35 @@ def duita_num_joda():
 duita_num_joda()
 
 # taking input from user:
-my_name=input("Please enter your name-->: ")
-print("Ma sanga stored bhako value yei ho-->",my_name)
+# my_name=input("Please enter your name-->: ")
+# print("Ma sanga stored bhako value yei ho-->",my_name)
+
+
+f_name=input("Your first name:-->")
+l_name=input("Your second name:-->")
+
+def full_name_generator(first_name,second_name):
+    full_name = f"{first_name} {second_name}"
+    return full_name
+
+print("Your full name is:", full_name_generator(f_name,l_name))
+
+
+#string formatting
+# print("My-1 name-2 is-3 abc-4 def-5")
+
+phone_num = 984551
+print(f"Your phone number is {phone_num}")
+
+first =1 
+second =2 
+third =3
+four =4
+fifth = 5
+
+print(f"My-{first} name-{second} is-{third} abc-{four} def-{fifth}")
+
+
+# len function
+length_of_this = len("Education")
+print(length_of_this)
